8d9d83d173a6b9eabbc7904a2c9d23dbee24d240
Daniel Loyd <dhloyd@icloud.com>
2020-02-02 08:57:28 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,13 @@
+array_sample = [1,2,3,4]
+
 def square_array(array)
-  # your code here
+count = 0
+new_array =[]
+  while count < array.size do
+    new_array << array[count] ** 2
+    count +=1
+  end
+new_array
 end
+
+square_array(array_sample)
